865528400eaf95481befde03ccb0c3b1bf8ae345
fp-ming <fp-ming@qq.com>
2021-04-24 22:50:41 +0800

    change file

--- src/components/split/Split.vue
+++ src/components/split/Split.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="split"></div>
+  <div class="split"></div>
 </template>
 
 <script>
-    export default {
-        name: "Split"
-    }
+  export default {
+    name: "Split"
+  }
 </script>
 
 <style scoped lang="stylus">
   .split
-    width : 100%
-    height : 16px
-    background : #f3f5f7
-    border-top : 1px solid rgba(7,17,27,.1)
-    border-bottom : 1px solid rgba(7,17,27,.1)
+    width: 100%
+    height: 16px
+    background: #f3f5f7
+    border-top: 1px solid rgba(7, 17, 27, .1)
+    border-bottom: 1px solid rgba(7, 17, 27, .1)
 </style>
